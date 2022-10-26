14c9929a555b14d975837fe16cebe299b421c2a6
Gordon Cheung <gordonhwc1@gmail.com>
2022-10-25 23:30:01 -0700

    t2pg-2 add render_to_pdf

--- backend/backend/utils.py
+++ backend/backend/utils.py
@@ -1,5 +1,10 @@
 from django.template.base import VariableNode, Template 
 import io
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+
+
 
 class Utils():
     def get_variable_nodes(file):
@@ -12,3 +17,12 @@ class Utils():
         node_names = [n.filter_expression.token for n in nodes]
         
         return node_names
+    
+    def render_to_pdf(template_src, context_dict={}):
+        template = get_template(template_src)
+        html  = template.render(context_dict)
+        result = io.BytesIO()
+        pdf = pisa.pisaDocument(BytesIO(html.encode("ISO-8859-1")), result)
+        if not pdf.err:
+            return HttpResponse(result.getvalue(), content_type='application/pdf')
+        return None
