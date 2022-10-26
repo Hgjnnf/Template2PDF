3f1b3993935952432188fbf97798bafcd9fb24cf
Gordon Cheung <74426497+Hgjnnf@users.noreply.github.com>
2022-10-25 22:49:56 -0700

    [T2PG-1] Upload API (#1)
    * upload api complete
    
    * t2pg-1 change route

---
+++ backend/backend/views.py
@@ -0,0 +1,21 @@
+
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from .utils import Utils
+from .serializers import UploadSerializer
+
+class UploadViewSet(ViewSet):
+    serializer_class = UploadSerializer
+    
+    def create(self, request):
+        file_uploaded = request.FILES.get('file_uploaded')
+        
+        if not file_uploaded:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        if file_uploaded.content_type != 'text/html':
+            return Response(status=status.HTTP_406_NOT_ACCEPTED)
+
+        response = Utils.get_variable_nodes(file_uploaded.file)
+        return Response(response)
