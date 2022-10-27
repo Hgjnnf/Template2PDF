fd198d3bd49295391e6c0ba21dbb8cdebc099bec
Gordon Cheung <74426497+Hgjnnf@users.noreply.github.com>
2022-10-26 23:36:45 -0700

    [t2pg-2] Download API (#2)
    * pull from remote master
    
    * t2pg-2 add render_to_pdf
    
    * t2pg-2 add download serializer
    
    * t2pg-2 set up download viewset
    
    * t2pg-2 finalize serializer
    
    * t2pg-2 controller complete
    
    * t2pg-2 debug complete
    
    * t2pg-2 fix status code

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -1,9 +1,10 @@
-
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import HttpResponse
+import json
 from .utils import Utils
-from .serializers import UploadSerializer
+from .serializers import UploadSerializer, DownloadSerializer
 
 class UploadViewSet(ViewSet):
     serializer_class = UploadSerializer
@@ -15,7 +16,35 @@ class UploadViewSet(ViewSet):
             return Response(status=status.HTTP_404_NOT_FOUND)
         
         if file_uploaded.content_type != 'text/html':
-            return Response(status=status.HTTP_406_NOT_ACCEPTED)
+            return Response(status=status.HTTP_406_NOT_ACCEPTABLE)
 
         response = Utils.get_variable_nodes(file_uploaded.file)
-        return Response(response)
+        return Response(response, status=status.HTTP_200_OK)
+
+class DownloadViewSet(ViewSet):
+    serializer_class = DownloadSerializer
+
+    def create(self, request):
+        file_uploaded = request.FILES.get('file_uploaded')
+        context_obj = request.data.get('context_obj')
+        file_name = request.data.get('file_name')
+
+        if not file_uploaded:
+            return Response("No file was uploaded", status=status.HTTP_404_NOT_FOUND)
+        if file_uploaded.content_type != 'text/html':
+            return Response("Wrong file type", status=status.HTTP_406_NOT_ACCEPTABLE)
+        if not context_obj:
+            return Response("Missing context", status=status.HTTP_404_NOT_FOUND)
+        if not file_name:
+            return Response("Missing file name", status=status.HTTP_404_NOT_FOUND)
+            
+        context_dict = json.loads(context_obj)
+        pdf = Utils.render_to_pdf(file_uploaded, context_dict)
+        file_name += '.pdf'
+
+        if pdf:
+            response = HttpResponse(pdf, content_type='application/pdf', status=status.HTTP_200_OK)
+            content = "attachment; filename=%s" %(file_name)
+            response['Content-Disposition'] = content
+            return response
+        return Response("File download failed", status=status.HTTP_404_NOT_FOUND)
