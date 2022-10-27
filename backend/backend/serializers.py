3674afbff80054b3642a28000e6e53c86538e126
Gordon Cheung <gordonhwc1@gmail.com>
2022-10-26 18:53:15 -0700

    t2pg-2 add download serializer

--- backend/backend/serializers.py
+++ backend/backend/serializers.py
@@ -1,7 +1,13 @@
-from rest_framework.serializers import Serializer, FileField
+from rest_framework.serializers import Serializer, FileField, DictField
 
 # Serializers define the API representation.
 class UploadSerializer(Serializer):
     file_uploaded = FileField()
     class Meta:
         fields = ['file_uploaded']
+
+class DownloadSerializer(Serializer):
+    file_downloaded = FileField()
+    context_obj = DictField()
+    class Meta:
+        fields = ['file_downloaded', 'context_obj']
