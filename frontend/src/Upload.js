98fd8534afb2187473027fe70a7723c3808953a5
Gordon Cheung <gordonhwc1@gmail.com>
2022-10-30 00:27:46 -0700

    t2pg-3 ui styling done

--- frontend/src/Upload.js
+++ frontend/src/Upload.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useDropzone } from "react-dropzone";
 import "./Upload.css";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
+import Button from "@mui/material/Button";
+import { AppBar, Toolbar } from "@mui/material";
 
 const Upload = (props) => {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
@@ -17,14 +20,23 @@ const Upload = (props) => {
 
   return (
     <div className="main">
-      <h1 id="title">HTML To PDF Generator</h1>
+      <AppBar position="static" className="top-bar">
+        <Toolbar variant="dense">
+          <h1>Template to PDF Generator</h1>
+        </Toolbar>
+      </AppBar>
       <section className="file-drop">
         <div {...getRootProps({ className: "dropzone" })}>
           <input {...getInputProps()} />
           <p>Drag and drop the html file here, or click to select the file</p>
         </div>
-        <h4>File:</h4>
-        {files}
+        <div className="file-drop-bottom">
+          <div className="file">
+            <DescriptionOutlinedIcon />
+            {files}
+          </div>
+          <Button variant="contained">Upload</Button>
+        </div>
       </section>
     </div>
   );
