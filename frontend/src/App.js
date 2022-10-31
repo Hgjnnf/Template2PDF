027d25b0125d5b1d60c28589d1295490c3986f07
Gordon Cheung <gordonhwc1@gmail.com>
2022-10-31 14:50:35 -0700

    t2pg-4 complete download ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Upload from "./Upload";
+import Download from "./Download";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Upload />} />
-        {/* <Route path="/download" element={<Download />}> */}
+        <Route path="/download" element={<Download />} />
       </Routes>
     </Router>
   );
