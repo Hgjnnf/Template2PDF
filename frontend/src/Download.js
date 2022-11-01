aadc349cc840dd3e1df97bc8316860fdb07f9861
Gordon Cheung <74426497+Hgjnnf@users.noreply.github.com>
2022-11-01 00:13:54 -0700

    [t2pg-5] Frontend Logic (#5)
    * pull from remote master
    
    * t2pg-5 add context
    
    * t2pg-5 logic for upload
    
    * t2pg-5 debug upload logic
    
    * t2pg-5 logic for download
    
    * t2pg-5 remove comments
    
    * t2pg-5 debug download logic

--- frontend/src/Download.js
+++ frontend/src/Download.js
@@ -1,8 +1,49 @@
 import { Button, TextField } from "@mui/material";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { context } from "./Context";
 import "./Download.css";
 
 const Download = () => {
-  const mockData = ["name", "amount", "date"];
+  const [file_name, setFileName] = useState("");
+  const [context_obj, setContextObj] = useState({});
+  const file = localStorage.getItem("file");
+  const { varList, clearList } = useContext(context);
+  let navigate = useNavigate();
+
+  const handleChange = (key, value) => {
+    if (key === "filename") {
+      setFileName(value);
+    } else {
+      const new_obj = context_obj;
+      new_obj[key] = value;
+      setContextObj(new_obj);
+    }
+  };
+
+  const handleClick = () => {
+    if (Object.keys(context_obj).length !== varList.length) {
+      alert("Missing field!");
+      return;
+    }
+
+    if (!file_name) {
+      alert("Missing file name!");
+      return;
+    }
+    // API
+    const res = {
+      data: "",
+      status: 200,
+    };
+
+    if (res.status !== 200) {
+      alert(res.data);
+    }
+    clearList();
+    localStorage.clear();
+    navigate("/");
+  };
 
   return (
     <div className="main-download">
@@ -11,27 +52,31 @@ const Download = () => {
       </span>
       <div className="inputs">
         <TextField
-          required
+          required={true}
           id="filled-required"
           label="filename"
           variant="filled"
           key={0}
+          onChange={(e) => handleChange("filename", e.target.value)}
         />
         <span className="divider"></span>
-        {mockData.map((el, idx) => (
+        {varList.map((el, idx) => (
           <>
             <TextField
-              required
+              required={true}
               id="filled-required"
               label={el}
               variant="filled"
               key={idx + 1}
+              onChange={(e) => handleChange(el, e.target.value)}
             />
             <span className="divider"></span>
           </>
         ))}
       </div>
-      <Button variant="contained">Download</Button>
+      <Button variant="contained" onClick={() => handleClick()}>
+        Download
+      </Button>
     </div>
   );
 };
