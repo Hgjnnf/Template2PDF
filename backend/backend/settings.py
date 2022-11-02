3c6254833043ba399fba09def7033babbb068a4b
Gordon Cheung <gordonhwc1@gmail.com>
2022-11-01 17:43:45 -0700

    t2pg-6 set up cors

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+FE_DOMAIN = os.environ['FE_DOMAIN']
+CORS_ALLOWED_ORIGINS = [
+    FE_DOMAIN,
 ]
 
 ROOT_URLCONF = 'backend.urls'
