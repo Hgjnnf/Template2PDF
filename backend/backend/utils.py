8edde641f1d4abde78da390b50728e403c78ded0
Gordon Cheung <74426497+Hgjnnf@users.noreply.github.com>
2022-11-06 18:27:29 -0800

    [t2pg-8] Debug (#9)
    * pull from remote master
    
    * t2pg-8 debug duplicate variable template

--- backend/backend/utils.py
+++ backend/backend/utils.py
@@ -17,9 +17,12 @@ class Utils():
         t = Template(file_obj)
         nodes = t.nodelist.get_nodes_by_type(VariableNode)
 
-        node_names = [n.filter_expression.token for n in nodes]
+        node_names = set()
+
+        for n in nodes:
+            node_names.add(n.filter_expression.token)
         
-        return node_names
+        return list(node_names)
     
     def render_to_pdf(template_src, context_dict={}):
         file_obj = Utils.convert_file(template_src)
